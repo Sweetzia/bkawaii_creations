1555832a2f08eec57bc0db1b59e66c659f4ff4ff
Sweetzia <marjoleintje@mail.com>
2021-02-13 09:53:50 +0000

    added products app and models

---
+++ products/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+# Used the code from the Code Institute Project - Boutique Ado - 
+# and adjusted this code for my own web application.
+
+
+class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=200)
+    friendly_name = models.CharField(max_length=200, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Product(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=200, null=True, blank=True)
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
