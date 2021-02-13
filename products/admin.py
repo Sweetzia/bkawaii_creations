ac0752559bb5f840a5588c90c8f8f4decc83aa15
Sweetzia <marjoleintje@mail.com>
2021-02-13 10:01:24 +0000

    imported product and categories in admin.py

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from .models import Product, Category
 
 # Register your models here.
+# Used the code from the Code Institute Project - Boutique Ado -
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
+© 2021 GitHub, Inc.
+Terms
+Privacy
+Security
