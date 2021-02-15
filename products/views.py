f128f153025852ca8fa84d195f44deece18202bd
Sweetzia <marjoleintje@mail.com>
2021-02-15 10:14:45 +0000

    added category filtering

--- products/views.py
+++ products/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product
+from .models import Product, Category
 
 # Create your views here.
+# Used the code from the Code Institute Project - Boutique Ado -
 
 
 def all_products(request):
@@ -11,21 +12,52 @@ def all_products(request):
 
     products = Product.objects.all()
     query = None
+    categories = None
+    sort = None
+    direction = None
 
+# Sort the products by price, rating and category
+    if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+# Annotation allows to add a temporary field on a model
+                products = products.annotate(lower_name=Lower('name'))
+# Sort categories by name instead of ids. Sort key is equal to category
+            if sortkey == 'category':
+                sortkey = 'category__name'
+# Direction ascending from highest to lowest price or rating
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+                    # Sort products
+            products = products.order_by(sortkey)
+
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
 # Using Q to construct search query and return results when query is matched.         
-    if 'q' in request.GET:
-        query = request.GET['q']
-        if not query:
-            messages.error(request, "You didn't enter any search criteria!")
-            return redirect(reverse('products'))
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('products'))
 
 # Using Q to construct search query and return results when query is matched.
-        queries = Q(name__icontains=query) | Q(description__icontains=query)
-        products = products.filter(queries)
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
+# Sort products in string formatting
+    current_sorting = f'{sort}_{direction}'
 # Context manager
     context = {
         'products': products,
         'search_term': query,
+        'current_categories': categories,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'products/products.html', context)
@@ -37,7 +69,7 @@ def product_detail(request, product_id):
     product = get_object_or_404(Product, pk=product_id)
 
     context = {
-        'product': product,
+        'product': product
     }
 
     return render(request, 'products/product_detail.html', context)
