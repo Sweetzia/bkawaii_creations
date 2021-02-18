fb15c9985bef11a4456f277b71ffe58c2690cf9f
Sweetzia <marjoleintje@mail.com>
2021-02-18 10:15:28 +0000

    added checkout.css, template and created checkout view + urls

---
+++ checkout/urls.py
@@ -0,0 +1,7 @@
+# Used the code from the Code Institute Project - Boutique Ado -
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.checkout, name='checkout')
+]
