7d2dcdb6925400e8c01ff0ae961800e6bba10250
Sweetzia <marjoleintje@mail.com>
2021-02-18 09:49:16 +0000

    checkout app admin and django signals

---
+++ checkout/signals.py
@@ -0,0 +1,23 @@
+# Used the code from the Code Institute Project - Boutique Ado -
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+
+from .models import OrderLineItem
+
+
+# receiver decorator telling we're receiving post save signals from the order line item model
+@receiver(post_save, sender=OrderLineItem)
+# function handle signals from the post_save event
+def update_on_save(sender, instance, created, **kwargs):
+    """
+    Update order total on lineitem update/create
+    """
+    instance.order.update_total()
+
+# receiver decorator telling we're receiving post delete signals from the order line item model
+@receiver(post_delete, sender=OrderLineItem)
+def update_on_save(sender, instance, **kwargs):
+    """
+    Update order total on lineitem delete
+    """
+    instance.order.update_total()
