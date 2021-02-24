aff4d4afab7665d20c46bc7334bc313d41185e77
Sweetzia <marjoleintje@mail.com>
2021-02-24 19:17:43 +0000

    added profile form, views and updated template

---
+++ profiles/forms.py
@@ -0,0 +1,40 @@
+# Used the code from the Code Institute Project - Boutique Ado - 
+from django import forms
+from .models import UserProfile
+
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        exclude = ('user',)
+
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders and classes, remove auto-generated
+        labels and set autofocus on first field
+        """
+        # *args and **kwargs are mostly used in function definitions. 
+        # *args and **kwargs allow you to pass an unspecified number of arguments to a function, 
+        # so when writing the function definition, you do not need to know how many arguments will be passed to your function. 
+        # *args is used to send a non-keyworded variable length argument list to the function.
+        # https://book.pythontips.com/en/latest/args_and_kwargs.html
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'default_phone_number': 'Phone Number',
+            'default_postcode': 'Postal Code',
+            'default_town_or_city': 'Town or City',
+            'default_street_address1': 'Street Address 1',
+            'default_street_address2': 'Street Address 2',
+            'default_county': 'County, State or Locality',
+        }
+
+        self.fields['default_phone_number'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if field != 'default_country':
+                if self.fields[field].required:
+                    placeholder = f'{placeholders[field]} *'
+                else:
+                    placeholder = placeholders[field]
+                self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'border-black rounded-0 profile-form-input'
+            self.fields[field].label = False
