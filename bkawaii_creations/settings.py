e1d018b73cbbc215206edd59deb627a08f62d1a8
Sweetzia <marjoleintje@mail.com>
2021-03-01 18:27:00 +0000

    SECRET_KEY changed

--- bkawaii_creations/settings.py
+++ bkawaii_creations/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('cq8v=b5unz0$x5^mp=3$e5r*j6lgh71mtfq_2&v69f^6lo&==r', '')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
