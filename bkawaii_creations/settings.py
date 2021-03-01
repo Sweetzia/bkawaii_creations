ae12bb56e8e0f97ebf570212b84546967ed4dccd
Sweetzia <marjoleintje@mail.com>
2021-03-01 17:39:42 +0000

    changed SECRET_KEY

--- bkawaii_creations/settings.py
+++ bkawaii_creations/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(dp9j7^@hu!=_zjkxpbfhe@3)8(-3p^674fv=o_&n-4k3*)3a$'
+SECRET_KEY = os.environ.get('SECRET_KEY, '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['bkawaii-creations.herokuapp.com, localhost']
+ALLOWED_HOSTS = ['bkawaii-creations.herokuapp.com', 'localhost']
 
 
 # Application definition
