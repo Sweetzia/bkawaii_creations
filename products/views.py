b5537bea893beab5389a46211b813c843f6b1814
Sweetzia <marjoleintje@mail.com>
2021-03-02 08:37:31 +0000

    changed login and logout template

--- products/views.py
+++ products/views.py
@@ -87,7 +87,7 @@ def add_product(request):
             messages.success(request, 'Successfully added product!')
             return redirect(reverse('product_detail', args=[product.id]))
         else:
-            messages.error(request, 'Failed to add product. Please ensure the form is valid.')
+            messages.error(request, 'Failed to add product.')
     else:
         form = ProductForm()
         
@@ -109,7 +109,7 @@ def edit_product(request, product_id):
             messages.success(request, 'Successfully updated product!')
             return redirect(reverse('product_detail', args=[product.id]))
         else:
-            messages.error(request, 'Failed to update product. Please ensure the form is valid.')
+            messages.error(request, 'Failed to update product')
     else:
         form = ProductForm(instance=product)
         messages.info(request, f'You are editing {product.name}')
