ececc5fefcb1a4478cba3e81b3ffcebd9e9b2ce0
Sweetzia <marjoleintje@mail.com>
2021-03-04 20:40:16 +0000

    changed static files code in settings.py

--- bkawaii_creations/settings.py
+++ bkawaii_creations/settings.py
@@ -167,13 +167,16 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.1/howto/static-files/
+# https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
-MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Stripe
 FREE_DELIVERY_THRESHOLD = 50
