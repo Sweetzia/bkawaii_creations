c8967af69a45e77f0fd3ef991610a3071e74902b
Sweetzia <marjoleintje@mail.com>
2021-03-04 18:52:57 +0000

    added extra place for collectstatic to find static files

--- bkawaii_creations/settings.py
+++ bkawaii_creations/settings.py
@@ -195,6 +195,11 @@ if 'USE_AWS' in os.environ:
     DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
     MEDIAFILES_LOCATION = 'media'
 
+    # Extra places for collectstatic to find static files.
+    STATICFILES_DIRS = (
+        os.path.join(BASE_DIR, 'static'),
+    )
+
     # Override static and media URLs in production
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
